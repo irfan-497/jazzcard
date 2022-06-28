3616d0e53b0db254056061a28f98c050f40fb6d0
Irfan Akhtar <irfangujjar3499@gmail.com>
2022-06-28 09:46:48 +0500

    Personal

---
+++ index.js
@@ -0,0 +1,45 @@
+// let n=5;
+// let string="";
+// for(let i=0; i<n; i++){
+//     for( let k=0; k<n-i; k++){
+//         string+= "*";
+//     }
+// } 
+// console.log(string);
+
+// let n=5;
+// let string="";
+// for(let i=0; i<n; i++){
+//     for(let k=0;k<n-i;k++){
+//         string+="*"
+//    }
+// }
+// console.log(string);
+// *******************************************************************************************************************
+
+// variable Declaration
+
+
+// var name=33;
+// console.log(name);
+
+// let n = 5;
+// let string = "";
+
+// for (let i = 0; i < n; i++) {
+  
+//   for (let k = 0; k < n - i; k++) {
+//     string += "*";
+//   }
+// }
+// console.log(string);
+
+
+// const age=33;
+// if(age<=20){
+//   console.log("young man");
+// }else{
+//   console.log("jawan");
+
+
+var greetinfg
---
+++ task.js
@@ -0,0 +1,47 @@
+// var value='';
+// console.log(typeof value)
+
+
+// const value=["i", "love", "pakistan"]
+// value.reverse();
+// console.log(value);
+
+// const string = prompt('hello ');
+
+// const result = reverseString(string);
+// console.log(result);
+
+
+// const name="irfan";{
+//     console.log(name);{
+//         if(true){
+//             name="gujjar"
+//         }
+//     }
+// }
+// console.log(name);
+
+// const value=44;
+// if(true){
+//     const value=44;
+// }
+// console.log(value);
+
+// const name=55;
+// function hello(){
+//     console.log(name);
+// }
+// hello();
+
+// function name(){
+//     const name=77;{
+//         console.log(value);
+//     }
+// }
+// name();
+
+// const Value=null;
+// console.log(typeof myNullValue);
+// u
+// let n=5;
+// let 
